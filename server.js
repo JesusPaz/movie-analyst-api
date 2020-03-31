6a0a9ba313dd893e31b4ef0772a8e4115bb961d2
Jesus Paz <jeherpabo@gmail.com>
2020-03-31 16:53:58 -0500

    Movies endpoint consumes databse

--- server.js
+++ server.js
@@ -1,23 +1,15 @@
 // Get our dependencies
 var express = require('express');
 var app = express();
-// var mysql = require("mysql");
-// var connection = mysql.createConnection({
-//  host     : process.env.DB_HOST || 'localhost',
-//  user     : process.env.DB_USER || 'applicationuser',
-//  password : process.env.DB_PASS || 'applicationuser',
-//  database : process.env.DB_NAME || 'movie_db'
-// });
+var mysql = require("mysql");
+var connection = mysql.createConnection({
+ host     : process.env.DB_HOST || 'localhost',
+ user     : process.env.DB_USER || 'applicationuser',
+ password : process.env.DB_PASS || 'applicationuser',
+ database : process.env.DB_NAME || 'movie_db'
+});
 
-// connection.connect();
-
-//function getMovies(callback) {    
-//        connection.query("SELECT * FROM movie_db.movies",
-//            function (err, rows) {
-//                callback(err, rows); 
-//            }
-//        );    
-//}
+connection.connect();
 
 //Testing endpoint
 app.get('/', function(req, res){
@@ -26,28 +18,21 @@ app.get('/', function(req, res){
 })
 
 // Implement the movies API endpoint
-app.get('/movies', function(req, res){
-  var movies = [
-    {title : 'Suicide Squad', release: '2016', score: 8, reviewer: 'Robert Smith', publication : 'The Daily Reviewer'},    
-    {title : 'Batman vs. Superman', release : '2016', score: 6, reviewer: 'Chris Harris', publication : 'International Movie Critic'},
-    {title : 'Captain America: Civil War', release: '2016', score: 9, reviewer: 'Janet Garcia', publication : 'MoviesNow'},
-    {title : 'Deadpool', release: '2016', score: 9, reviewer: 'Andrew West', publication : 'MyNextReview'},
-    {title : 'Avengers: Age of Ultron', release : '2015', score: 7, reviewer: 'Mindy Lee', publication: 'Movies n\' Games'},
-    {title : 'Ant-Man', release: '2015', score: 8, reviewer: 'Martin Thomas', publication : 'TheOne'},
-    {title : 'Guardians of the Galaxy', release : '2014', score: 10, reviewer: 'Anthony Miller', publication : 'ComicBookHero.com'},
-  ]
+function getMovies(callback) {    
+  connection.query("SELECT * FROM movie_db.moviereview",
+      function (err, rows) {
+          callback(err, rows); 
+      }
+  );    
+}
 
-  res.json(movies);
-})
+app.get('/movies', function(req, res, next) {   
+   getMovies(function (err, moviesResult){ 
+      //  you might want to do something is err is not null...      
+      res.json(moviesResult);
 
-//app.get('/', function(req, res, next) {   
-    //now you can call the get-driver, passing a callback function
-//    getMovies(function (err, moviesResult){ 
-       //you might want to do something is err is not null...      
-//       res.json(moviesResult);
-
-//    });
-//});
+   });
+});
 
 // Implement the reviewers API endpoint
 app.get('/reviewers', function(req, res){
